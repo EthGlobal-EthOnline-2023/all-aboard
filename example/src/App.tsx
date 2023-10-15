dda0643475c9ea1e1d3fa4de64b6c7e47f703c93
Firdaus <fai14@pitt.edu>
2023-10-15 17:13:11 -0400

    refactor: remove multiply

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'all-aboard-onboarding-rn';
+// import { multiply } from 'all-aboard-onboarding-rn';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    // multiply(3, 7).then(setResult);
   }, []);
 
   return (
